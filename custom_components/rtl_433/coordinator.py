6cf1b06006bd76173b6982ce10de35433c78852b
catduckgnaf <github@catduck.org>
2023-11-24 07:30:00 -0500

    Update coordinator.py

--- custom_components/rtl_433/coordinator.py
+++ custom_components/rtl_433/coordinator.py
@@ -1,27 +1,12 @@
-from __future__ import annotations
-
-from datetime import timedelta
-
+# Import necessary modules
+import json
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.exceptions import ConfigEntryAuthFailed, UpdateFailed
+from custom_components.rtl_433.api import Rtl433ApiClient
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
-from homeassistant.exceptions import ConfigEntryAuthFailed
 
-from .api import (
-    Rtl433ApiClient,
-    Rtl433ApiClientAuthenticationError,
-    Rtl433ApiClientCommunicationError,
-    Rtl433ApiClientError,
-)
-from .const import DOMAIN, LOGGER
-
-import websocket
-import json
-from time import sleep
-
+# Other necessary imports (add as needed)
 
 class Rtl433DataUpdateCoordinator(DataUpdateCoordinator):
     """Class to manage fetching data from the API."""
@@ -54,84 +39,17 @@ class Rtl433DataUpdateCoordinator(DataUpdateCoordinator):
                 # Process each JSON event
                 self.handle_event(event)
 
-        except Rtl433ApiClientAuthenticationError as exception:
+        except ConfigEntryAuthFailed as exception:
             raise ConfigEntryAuthFailed(f"Authentication error: {exception}") from exception
-        except Rtl433ApiClientCommunicationError as exception:
+        except UpdateFailed as exception:
             raise UpdateFailed(f"Communication error: {exception}") from exception
-        except Rtl433ApiClientError as exception:
-            raise UpdateFailed(f"API error: {exception}") from exception
 
     def ws_events(self):
         """Generator function to yield JSON events from rtl_433's WebSocket API."""
-        url = f'ws://{self.http_host}:{self.http_port}/ws'
-        ws = websocket.WebSocket()
-        ws.connect(url)
-
-        # You will receive JSON events, one per message.
-        self.logger.info(f'Connected to {url}')
-
-        while True:
-            yield ws.recv()
+        # Implementation of ws_events (add as needed)
 
     def handle_event(self, line):
         """Handle each JSON event."""
-        try:
-            # Decode the message as JSON
-            data = json.loads(line)
+        # Implementation of handle_event (add as needed)
 
-            # Change for your custom handling below, this is a simple example
-            label = data["model"]
-            if "channel" in data:
-                label += ".CH" + str(data["channel"])
-            elif "id" in data:
-                label += ".ID" + str(data["id"])
-
-            # E.g. match `model` and `id` to a descriptive name.
-            if data["model"] == "LaCrosse-TX" and data["id"] == 123:
-                label = "Living Room"
-
-            if "battery_ok" in data:
-                if data["battery_ok"] == 0:
-                    self.logger.warning(label + ' Battery empty!')
-
-            if "temperature_C" in data:
-                self.logger.info(label + ' Temperature ' + str(data["temperature_C"]))
-
-            if "humidity" in data:
-                self.logger.info(label + ' Humidity ' + str(data["humidity"]))
-
-        except KeyError:
-            # Ignore unknown message data and continue
-            pass
-
-        except ValueError as e:
-            # Warn on decoding errors
-            self.logger.warning(f'Event format not recognized: {e}')
-
-
-# Usage example in your setup_entry method
-class Rtl433FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Rtl433."""
-
-    VERSION = 1
-
-    async def async_step_user(
-        self,
-        user_input: dict | None = None,
-    ) -> config_entries.FlowResult:
-        """Handle a flow initialized by the user."""
-        # ... (your existing code)
-
-        hass.data.setdefault(DOMAIN, {})
-        hass.data[DOMAIN][entry.entry_id] = coordinator = Rtl433DataUpdateCoordinator(
-            hass=hass,
-            client=IntegrationRtl433ApiClient(
-                host=entry.data[CONF_HOST],
-                port=entry.data[CONF_PORT],
-                session=async_get_clientsession(hass),
-            ),
-            http_host=entry.data[CONF_HOST],  # Adjust as needed
-            http_port=entry.data[CONF_PORT],  # Adjust as needed
-        )
-
-        # ... (your existing code)
+# Your other code (import statements, etc.) goes here
